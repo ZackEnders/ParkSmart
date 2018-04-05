13e4aa1dea79303384801d0229e49a509d7fab43
Zack Enders <enders.zack@gmail.com>
2018-04-05 13:26:14 -0400

    Updated some styling with google maps and cleaned up some code

--- app/controllers/parkingspots_controller.rb
+++ app/controllers/parkingspots_controller.rb
@@ -14,17 +14,19 @@ class ParkingspotsController < ApplicationController
     @reservation = Reservation.new
     @reservations = Reservation.where(parkingspot_id: @parkingspot.id)
     @unavailableDates = []
+    #Stores the reservation dates in an array
       @reservations.each do |t|
         @unavailableDates.push({start_date: t.start_date.strftime('%Y-%m-%d'), end_date: t.end_date.strftime('%Y-%m-%d')})
       end
+      #Converts the reservation dates to json and then parses the data which allows for sorting
         @data = @unavailableDates.to_json
         @object = JSON.parse(@data, object_class: OpenStruct)
     @reservedDates = []
+    #Pushes the date results into an array 
       @object.each do |o|
         @reservedDates.push(o.start_date)
         @reservedDates.push(o.end_date)
       end
-
   end
 
   def create
@@ -67,11 +69,11 @@ class ParkingspotsController < ApplicationController
   def parkingspot_params
     params.require(:parkingspot).permit(
        	:user_id, :address, :price, :rented, 
-       	:description, :latitude, :longitude, :listing_name
-    )
+       	:description, :latitude, :longitude, :listing_name)
   end
   def reservation_params
-    params.require(:reservation).permit(:start_date, :end_date, :price, :total, :parkingspot_id)
+    params.require(:reservation).permit(:start_date, :end_date,
+       :price, :total, :parkingspot_id)
   end
 
 end
--- app/models/parkingspot.rb
+++ app/models/parkingspot.rb
@@ -1,10 +1,17 @@
 class Parkingspot < ApplicationRecord
+#Relations
 	belongs_to :user
 	has_many :reservations
 	# has_many :photos
 
+#Geocoder
 	geocoded_by :address
 	after_validation :geocode, if: :address_changed?
- 	validates :listing_name, length: {maximum: 50}
- 	validates :description, length: {maximum: 500}
+
+#Parkingspot validations
+ 	validates :listing_name, length: {maximum: 50}, presence: true
+ 	validates :description, length: {maximum: 500}, presence: true
+ 	validates :address, presence: true
+  validates :user_id, presence: true
+  validates :price, presence: true
 end
